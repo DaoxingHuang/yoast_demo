7c6080f4515fa5c92da2985a4551468df05d9dff
Daoxing.Huang <daoxing.huang@DaoxingHuangs-MacBook-Pro.local>
2020-11-16 11:12:53 +0800

    fix depencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { AnalysisList } from "@yoast/analysis-report";
+import { Loader } from "@yoast/components";
+import {
+	BaseButton,
+	Button,
+	IconButton,
+	IconButtonToggle,
+	IconLabeledButton,
+	IconsButton,
+	LinkButton,
+	UpsellButton,
+	UpsellLinkButton,
+	YoastButton,
+	YoastLinkButton,
+} from "@yoast/components";
 function App() {
   return (
     <div className="App">
+      <AnalysisList results={["123","123"]}></AnalysisList>
+      <Loader/><YoastButton>123</YoastButton>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
